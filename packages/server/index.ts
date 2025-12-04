efa40212013b982cb59b765a20abb7425f31c3a8
Vishesh Singh <visheshsingh16@gmail.com>
2025-12-04 14:31:39 -0500

    manage openai api key 🗝️

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,9 +1,13 @@
 import express, { type Request, type Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
+    console.log(process.env.OPENAI_API_KEY);
     res.send('Hello world!!!!')
 })
 
