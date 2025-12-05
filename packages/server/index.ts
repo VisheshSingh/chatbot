ce6c3450cabcfeddd82e21c3a377aefe76db57f2
Vishesh Singh <visheshsingh16@gmail.com>
2025-12-05 17:38:53 -0500

    input validation with zod 🔍

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,6 +1,7 @@
 import express, { type Request, type Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
+import z from 'zod';
 
 dotenv.config();
 
@@ -14,7 +15,23 @@ app.get('/api/health', (req: Request, res: Response) => {
 
 const conversations = new Map<string, string>();
 
+const chatSchema = z.object({
+   prompt: z
+      .string()
+      .trim()
+      .min(1, 'Prompt is required')
+      .max(1000, 'Prompt cannot exceed 1000 chars'),
+   conversation_id: z.uuid(),
+});
+
 app.post('/api/chats', async (req: Request, res: Response) => {
+   const parsedRequest = chatSchema.safeParse(req.body);
+
+   if (!parsedRequest.success) {
+      res.status(400).json(parsedRequest.error.format);
+      return;
+   }
+
    const { prompt, conversation_id } = req.body;
 
    const client = new OpenAI({
